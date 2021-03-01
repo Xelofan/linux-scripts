8e80f0254e2da0020e0cee4a6846d8e98e0675db
Xelofan <39835534+Xelofan@users.noreply.github.com>
2021-03-01 13:06:07 +0100

    Update debian10-docker-installer.sh

--- debian10-docker-installer.sh
+++ debian10-docker-installer.sh
@@ -11,4 +11,5 @@ sudo usermod -aG docker ${USER}
 id -nG
 
 # + Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /us>sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
